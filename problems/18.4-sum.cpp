987895adee00a4beb7b1eb11cb2a7d3dd2d0144b
Jason Yi <jasonyi1413@gmail.com>
2025-12-02 02:07:19 +1100

    AC 4-sum

--- problems/18.4-sum.cpp
+++ problems/18.4-sum.cpp
@@ -8,30 +8,53 @@
 using namespace std;
 
 // @lc code=start
+// Very slop
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        if (nums.size() < 4) return {};
-        set<vector<int>> output;
+        vector<vector<int>> output;
+        if (nums.size() < 4) return output;
         
         sort(nums.begin(), nums.end());
+        unordered_map<int, int> containsNum;
+        for (int n : nums) containsNum[n] += 1;
 
         // cout << nums.size() << endl;
+        int lastIN {nums[0] - 1};
         for (int i {0}; i < nums.size() - 3; ++i) {
+            if (nums[i] == lastIN) continue;
+            lastIN = nums[i];
+
+            int lastJN {nums[i] - 1};
             for (int j {i + 1}; j < nums.size() - 2; ++j) {
+                if (nums[j] == lastJN) continue;
+                lastJN = nums[j];
+                
+                int lastKN {nums[j] - 1};
                 for (int k {j + 1}; k < nums.size() - 1; ++k) {
-                    for (int l {k + 1}; l < nums.size(); ++l) {
-                        // cout << i << " " << j << " " << k << " " << l << endl;
-                        vector<int> quadruplet = {nums[i], nums[j], nums[k], nums[l]};
-                        if (accumulate(quadruplet.begin(), quadruplet.end(), 0) == target) {
-                            output.insert(quadruplet);
-                        }
+                    if (nums[k] == lastKN) continue;
+                    lastKN = nums[k];
+                    
+                    vector<int> sol = {nums[i], nums[j], nums[k]};
+                    // int64_t solSum = accumulate(sol.begin(), sol.end(), 0);
+                    int64_t solSum = (int64_t)nums[i] + (int64_t)nums[j] + (int64_t)nums[k];
+                    int64_t limit = numeric_limits<int>::max();
+                    if (-limit > ((int64_t)target - solSum)) continue;
+                    if (limit < ((int64_t)target - solSum)) continue;
+                    
+                    int lastN = (int64_t)target - solSum;
+                    // cout << lastN << " " << solSum << " " << target << endl;
+                    if (lastN < nums[k]) continue;
+                    // cout << (containsNum[lastN] > count(sol.begin(), sol.end(), lastN)) <<  " " << count(sol.begin(), sol.end(), lastN) << " " << containsNum[lastN];
+                    if (containsNum[lastN] > 0 && containsNum[lastN] > count(sol.begin(), sol.end(), lastN)) {
+                        sol.push_back(lastN);
+                        output.push_back(sol);
                     }
                 }
             }
         }
 
-        return vector<vector<int>>(output.begin(), output.end());
+        return output;
     }
 };
 // @lc code=end
