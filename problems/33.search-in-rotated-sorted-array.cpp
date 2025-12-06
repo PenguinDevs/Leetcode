ad395bdb99fd06a24da6037824e5c17ce0d13112
Jason Yi <jasonyi1413@gmail.com>
2025-12-06 16:27:50 +1100

    AC Search in rotated sorted array

---
+++ problems/33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        unsigned l {0};
+        unsigned r = nums.size() - 1;
+
+        while (l < r) {
+            unsigned mid = (l + r) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (mid != 0 && nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target <= nums[mid]) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            } else if (mid != nums.size() - 1 && nums[r] >= nums[mid + 1]) {
+                if (nums[r] >= target && target >= nums[mid + 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            } else {
+                return -1;
+            }
+
+            // if (nums[l] <= target && target <= nums[mid - 1]) {
+            //     r = mid;
+            // } else if (nums[r] >= target && target >= nums[mid + 1]) {
+            //     l = mid + 1;
+            // } else {
+            //     return -1;
+            // }
+        }
+
+        return nums[l] == target ? l : -1;
+    }
+};
+// @lc code=end
+
