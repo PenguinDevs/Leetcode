1abd19c01675bd43ed4eaef49e7e5c209de72f35
Jason Yi <jasonyi1413@gmail.com>
2026-01-02 01:13:40 +1100

    Improve Reverse integer

--- problems/7.reverse-integer.cpp
+++ problems/7.reverse-integer.cpp
@@ -2,6 +2,9 @@
  * @lc app=leetcode id=7 lang=cpp
  *
  * [7] Reverse Integer
+ * 
+ * // Note: lesson learnt is that, leetcode problems don't usually involve
+ * this degree of complexity
  */
 
 #include <bits/stdc++.h>
@@ -12,41 +15,59 @@ using namespace std;
 class Solution {
 public:
     int reverse(int x) {
-        if (x == numeric_limits<int>::min()) return 0;
-        if (x == numeric_limits<int>::max()) return 0;
-
-        const int digitsNo = ceil(log10(1 + abs(x)));
-        static const int maxDigitsNo = ceil(log10(pow(2, 32)));
-
         int output {0};
-        bool isMax = digitsNo == maxDigitsNo;
-        for (int i {0}; i < digitsNo; ++i) {
-            const int digit = extractDigit(x, i);
 
-            if (isMax) {
-                const int maxDigit = extractDigit(numeric_limits<int>::max(), digitsNo - 1 - i);
-                // cout << maxDigit << " " << digit << endl;
-                if (abs(digit) > maxDigit) {
-                    return 0;
-                } else if (abs(digit) < maxDigit) {
-                    isMax = false;
-                }
+        while (x) {
+            // Don't need to worry about the leading digit causing overflow,
+            // since the first digit is <= 2 and last digit is 7 > 2.
+            if (output > numeric_limits<int>::max() / 10 || output < numeric_limits<int>::min() / 10) {
+                return 0;
             }
-
-            output += digit * pow(10, (digitsNo - 1 - i));
+            output = output * 10 + x % 10;
+            x /= 10;
         }
 
-        return output | (1 << 31 & x);
-    }
-private:
-    int extractDigit(int n, int i) {
-        static const int maxDigitsNo = ceil(log10(pow(2, 32)));
-        if (i == maxDigitsNo - 1) {
-            return (n - (n % static_cast<int>(pow(10, (i)))))/(static_cast<int>(pow(10, (i))));
-        } else {
-            return ((n % static_cast<int>(pow(10, (i + 1)))) - (n % static_cast<int>(pow(10, (i)))))/(static_cast<int>(pow(10, (i))));
-        }
+        return output;
     }
 };
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         if (x == numeric_limits<int>::min()) return 0;
+//         if (x == numeric_limits<int>::max()) return 0;
+
+//         const int digitsNo = ceil(log10(1 + abs(x)));
+//         static const int maxDigitsNo = ceil(log10(pow(2, 32)));
+
+//         int output {0};
+//         bool isMax = digitsNo == maxDigitsNo;
+//         for (int i {0}; i < digitsNo; ++i) {
+//             const int digit = extractDigit(x, i);
+
+//             if (isMax) {
+//                 const int maxDigit = extractDigit(numeric_limits<int>::max(), digitsNo - 1 - i);
+//                 // cout << maxDigit << " " << digit << endl;
+//                 if (abs(digit) > maxDigit) {
+//                     return 0;
+//                 } else if (abs(digit) < maxDigit) {
+//                     isMax = false;
+//                 }
+//             }
+
+//             output += digit * pow(10, (digitsNo - 1 - i));
+//         }
+
+//         return output | (1 << 31 & x);
+//     }
+// private:
+//     int extractDigit(int n, int i) {
+//         static const int maxDigitsNo = ceil(log10(pow(2, 32)));
+//         if (i == maxDigitsNo - 1) {
+//             return (n - (n % static_cast<int>(pow(10, (i)))))/(static_cast<int>(pow(10, (i))));
+//         } else {
+//             return ((n % static_cast<int>(pow(10, (i + 1)))) - (n % static_cast<int>(pow(10, (i)))))/(static_cast<int>(pow(10, (i))));
+//         }
+//     }
+// };
 // @lc code=end
 
