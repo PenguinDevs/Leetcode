567f18147929df86bf854e066b125a6b23b6b07f
Jason Yi <jasonyi1413@gmail.com>
2026-01-19 05:49:32 +1100

    AC Partition labels

---
+++ problems/763.partition-labels.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=763 lang=cpp
+ *
+ * [763] Partition Labels
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> charCount(26);
+        for (const char c : s) {
+            ++charCount[c - 'a'];
+        }
+
+        vector<int> partitionSizes;
+        int lastPartitionMark {-1};
+
+        vector<int> charTally(26);
+        for (int i {0}; i < s.size(); ++i) {
+            const char c = s[i];
+            const int cI = c - 'a';
+            if (charTally[cI] == 0) {
+                charTally[cI] = charCount[cI];
+            }
+            --charTally[cI];
+
+            if (all_of(charTally.begin(), charTally.end(), [](int v) { return v == 0; })) {
+                partitionSizes.push_back(i - lastPartitionMark);
+                lastPartitionMark = i;
+            }
+        }
+
+        return partitionSizes;
+    }
+};
+// @lc code=end
+
