966d3a940eb55050ead230814b00bbfb70d9a71c
Jason Yi <jasonyi1413@gmail.com>
2026-01-19 17:37:23 +1100

    AC Reverse string

---
+++ problems/344.reverse-string.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=344 lang=cpp
+ *
+ * [344] Reverse String
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for (int i {0}; i < s.size() / 2; ++i) {
+            swap(s[i], s[s.size() - i - 1]);
+        }
+    }
+};
+// @lc code=end
+
