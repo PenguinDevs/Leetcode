6ff71d5cbb10152c67563ceeb247dfbb3189c56a
Jason Yi <jasonyi1413@gmail.com>
2026-03-18 23:40:58 +1100

    AC Integer break

---
+++ problems/343.integer-break.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=343 lang=cpp
+ *
+ * [343] Integer Break
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int integerBreak(int n) {
+        auto canFit = [n](int target) -> bool {
+
+        };
+
+        int l {0};
+        int r {n};
+    }
+};
+// @lc code=end
+
