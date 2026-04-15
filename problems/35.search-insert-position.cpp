0070449ea3e11ba424457209d69a144269b47876
Jason Yi <jasonyi1413@gmail.com>
2026-04-16 02:18:27 +1000

    AC Search insert position

---
+++ problems/35.search-insert-position.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};
+// @lc code=end
+
