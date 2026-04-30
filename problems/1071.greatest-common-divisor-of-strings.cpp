7b55789f3014a11c99eba5fb9fc55876d61a0f3d
Jason Yi <jasonyi1413@gmail.com>
2026-05-01 00:40:41 +1000

    AC Greatest common divisor of strings

---
+++ problems/1071.greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=1071 lang=cpp
+ *
+ * [1071] Greatest Common Divisor of Strings
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(str1.length(), str2.length())) : "";
+    }
+};
+// @lc code=end
+
